ee63e53d0aead96ef363d57c552156480d28aeff
jackNEss <jackness1208@qq.com>
2018-09-10 17:10:30 +0800

    add make command

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const opzer = require('./lib/optimize.js');
 const init = require('./lib/init.js');
 const pkg = require('./package.json');
+const make = require('./lib/make.js');
 const cmd = {
   name: 'gulp-requirejs',
   version: pkg.version,
   path: __dirname,
   examples: init.examples,
   optimize: opzer,
-  init: init
+  init: init,
+  make: make
 };
 
 module.exports = cmd;
