43e43a4a5be2def56ffe65eed676c2f0f172757f
liudaojie <liudaojie@yy.com>
2020-03-02 18:36:57 +0800

    feat: 格式调整

--- test/handler.js
+++ test/handler.js
@@ -1,146 +1,146 @@
-const fs = require('fs');
-const path = require('path');
-const extFs = require('yyl-fs');
-const tUtil = require('yyl-seed-test-util');
-const print = require('yyl-print');
-const Hander = require('yyl-hander');
-const { Runner } = require('yyl-server');
-const chalk = require('chalk');
+const fs = require('fs')
+const path = require('path')
+const extFs = require('yyl-fs')
+const tUtil = require('yyl-seed-test-util')
+const print = require('yyl-print')
+const Hander = require('yyl-hander')
+const { Runner } = require('yyl-server')
+const chalk = require('chalk')
 
-const USERPROFILE = process.env[process.platform == 'win32' ? 'USERPROFILE' : 'HOME'];
-const WORKFLOW = 'gulp-requirejs';
-const RESOLVE_PATH = path.join(USERPROFILE, `.yyl/plugins/${WORKFLOW}`);
+const USERPROFILE = process.env[process.platform == 'win32' ? 'USERPROFILE' : 'HOME']
+const WORKFLOW = 'gulp-requirejs'
+const RESOLVE_PATH = path.join(USERPROFILE, `.yyl/plugins/${WORKFLOW}`)
 
 
-const seed = require('../index.js');
+const seed = require('../index.js')
 
 const yh = new Hander({
   log: function (type, status, args) {
     switch (type) {
-      case 'msg':
-        if (print.log[status]) {
-          print.log[status](args);
-        } else {
-          print.log.info(args);
-        }
-        break;
+    case 'msg':
+      if (print.log[status]) {
+        print.log[status](args)
+      } else {
+        print.log.info(args)
+      }
+      break
 
-      default:
-        break;
+    default:
+      break
     }
   },
   vars: {
     PROJECT_PATH: process.cwd()
   }
-});
+})
 
 const fn = {
   clearDest(config) {
     return new Promise((next) => {
       extFs.removeFiles(config.alias.destRoot).then(() => {
         extFs.copyFiles(config.resource).then(() => {
-          next();
-        });
-      });
-    });
+          next()
+        })
+      })
+    })
   },
   async initPlugins(config) {
     if (config.plugins && config.plugins.length) {
       if (!fs.existsSync(RESOLVE_PATH)) {
-        extFs.mkdirSync(RESOLVE_PATH);
+        extFs.mkdirSync(RESOLVE_PATH)
       }
-      await tUtil.initPlugins(config.plugins, RESOLVE_PATH);
+      await tUtil.initPlugins(config.plugins, RESOLVE_PATH)
     }
-    return config;
+    return config
   }
-};
+}
 
 const handler = {
   async all({ env }) {
-    print.log.setLogLevel( env.silent ? 0 : 2);
+    print.log.setLogLevel( env.silent ? 0 : 2)
 
-    let configPath;
-    let config;
+    let configPath
+    let config
 
     if (env.config) {
-      configPath = path.resolve(process.cwd(), env.config);
+      configPath = path.resolve(process.cwd(), env.config)
       if (!fs.existsSync(configPath)) {
-        return print.log.warn(`config path not exists: ${configPath}`);
+        return print.log.warn(`config path not exists: ${configPath}`)
       } else {
-        env.workflow = WORKFLOW;
-        config = await yh.parseConfig(configPath);
+        env.workflow = WORKFLOW
+        config = await yh.parseConfig(configPath)
       }
     } else {
-      return print.log.warn('task need --config options');
+      return print.log.warn('task need --config options')
     }
 
-    const PROJECT_PATH = path.dirname(configPath);
+    const PROJECT_PATH = path.dirname(configPath)
 
-    yh.setVars({ PROJECT_PATH });
-    yh.optimize.init({ config, iEnv: env });
-    await yh.optimize.initPlugins();
+    yh.setVars({ PROJECT_PATH })
+    yh.optimize.init({ config, iEnv: env })
+    await yh.optimize.initPlugins()
 
-    const opzer = seed.optimize(config, PROJECT_PATH);
+    const opzer = seed.optimize(config, PROJECT_PATH)
 
-    await extFs.removeFiles(config.alias.destRoot);
-    await extFs.removeFiles(config.resource);
+    await extFs.removeFiles(config.alias.destRoot)
+    await extFs.removeFiles(config.resource)
 
     await new Promise((next) => {
-      let isError = false;
+      let isError = false
       opzer.all(env)
         .on('msg', (type, ...argv) => {
-          let iType = type;
+          let iType = type
           if (!print.log[type]) {
-            iType = 'info';
+            iType = 'info'
           }
-          print.log[iType](...argv);
+          print.log[iType](...argv)
           if (type === 'error') {
-            isError = argv;
+            isError = argv
           }
         })
         .on('clear', () => {
           // print.cleanScreen();
         })
         .on('loading', (pkgName) => {
-          print.log.loading(`loading module ${chalk.green(pkgName)}`);
+          print.log.loading(`loading module ${chalk.green(pkgName)}`)
         })
         .on('finished', async() => {
           if (isError) {
-            print.log.error('task error', ...isError);
+            print.log.error('task error', ...isError)
           } else {
-            print.log.success('task finished');
+            print.log.success('task finished')
           }
-          next();
-        });
-    });
+          next()
+        })
+    })
 
-    return config;
+    return config
   },
   async watch({ env }) {
-    let config;
-    let configPath;
+    let config
+    let configPath
     if (env.silent) {
-      print.log.setLogLevel(0);
+      print.log.setLogLevel(0)
     } else {
-      print.log.setLogLevel(2);
+      print.log.setLogLevel(2)
     }
     if (env.config) {
-      configPath = path.resolve(process.cwd(), env.config);
+      configPath = path.resolve(process.cwd(), env.config)
       if (!fs.existsSync(configPath)) {
-        return print.log.warn(`config path not exists: ${configPath}`);
+        return print.log.warn(`config path not exists: ${configPath}`)
       } else {
-        env.workflow = WORKFLOW;
-        config = await yh.parseConfig(configPath, env);
+        env.workflow = WORKFLOW
+        config = await yh.parseConfig(configPath, env)
       }
     } else {
-      return print.log.warn('task need --config options');
+      return print.log.warn('task need --config options')
     }
 
-    const PROJECT_PATH = path.dirname(configPath);
-    yh.setVars({ PROJECT_PATH });
+    const PROJECT_PATH = path.dirname(configPath)
+    yh.setVars({ PROJECT_PATH })
 
-    yh.optimize.init({config, iEnv: env });
-    await yh.optimize.initPlugins();
+    yh.optimize.init({config, iEnv: env })
+    await yh.optimize.initPlugins()
 
     // 本地服务器
     const runner = new Runner({
@@ -148,24 +148,24 @@ const handler = {
       env,
       log(type, argu) {
         if (print.log[type]) {
-          print.log[type](...argu);
+          print.log[type](...argu)
         } else {
-          print.log.info(...argu);
+          print.log.info(...argu)
         }
       },
       cwd: PROJECT_PATH
-    });
+    })
 
-    await runner.start();
+    await runner.start()
 
-    config = runner.config;
+    config = runner.config
 
-    const opzer = seed.optimize(config, PROJECT_PATH);
+    const opzer = seed.optimize(config, PROJECT_PATH)
 
-    await fn.clearDest(config);
+    await fn.clearDest(config)
 
     await new Promise((next) => {
-      let isUpdate = false;
+      let isUpdate = false
       opzer.watch(env)
         .on('clear', () => {
           if (!env.silent) {
@@ -173,29 +173,29 @@ const handler = {
           }
         })
         .on('msg', (type, ...argv) => {
-          let iType = type;
+          let iType = type
           if (!print.log[type]) {
-            iType = 'info';
+            iType = 'info'
           }
           if (!env.silent) {
-            print.log[iType](...argv);
+            print.log[iType](...argv)
           }
         })
         .on('finished', async() => {
           if (!isUpdate) {
             if (!env.silent) {
-              yh.optimize.openHomePage();
+              yh.optimize.openHomePage()
             }
             // eslint-disable-next-line require-atomic-updates
-            isUpdate = true;
+            isUpdate = true
           }
-          next();
-        });
-    });
+          next()
+        })
+    })
     if (!env.silent) {
-      print.log.success('task finished');
+      print.log.success('task finished')
     }
   }
-};
+}
 
-module.exports = handler;
+module.exports = handler
--- test/mocha/all.case.base.test.js
+++ test/mocha/all.case.base.test.js
@@ -1,81 +1,80 @@
 /* eslint-disable prefer-arrow-callback */
 // WARNING 需要 连公司 vpn 再进行测试
-const path = require('path');
-const extFs = require('yyl-fs');
-const tUtil = require('yyl-seed-test-util');
-const handler = require('../handler');
+const path = require('path')
+const extFs = require('yyl-fs')
+const tUtil = require('yyl-seed-test-util')
+const handler = require('../handler')
 
 const {
-  clearDest,
   linkCheck,
   checkAsyncComponent,
   checkCssFiles
-} = require('../fn/all');
+} = require('../fn/all')
 
 
 // + vars
-const filename = 'base';
-const FRAG_PATH = path.join(__dirname, `../__frag/all-${filename}`);
-const TEST_CASE_PATH = path.join(__dirname, '../case');
-const PJ_PATH = path.join(TEST_CASE_PATH, filename);
-const COMMON_PATH = path.join(TEST_CASE_PATH, 'commons');
+const filename = 'base'
+const FRAG_PATH = path.join(__dirname, `../__frag/all-${filename}`)
+const TEST_CASE_PATH = path.join(__dirname, '../case')
+const PJ_PATH = path.join(TEST_CASE_PATH, filename)
+const COMMON_PATH = path.join(TEST_CASE_PATH, 'commons')
 // - vars
 
-tUtil.frag.init(FRAG_PATH);
+tUtil.frag.init(FRAG_PATH)
 describe(`seed.all() case:${filename}`, () => {
-  const TARGET_PATH = path.join(FRAG_PATH, filename);
-  const TARGET_COMMON_PATH = path.join(FRAG_PATH, 'commons');
-  const configPath = path.join(TARGET_PATH, 'config.js');
+  const TARGET_PATH = path.join(FRAG_PATH, filename)
+  const TARGET_COMMON_PATH = path.join(FRAG_PATH, 'commons')
+  const configPath = path.join(TARGET_PATH, 'config.js')
 
   before(async function () {
-    await tUtil.frag.build();
+    await tUtil.frag.build()
     await extFs.copyFiles(PJ_PATH, TARGET_PATH, (iPath) => {
-      const rPath = path.relative(PJ_PATH, iPath);
-      return !/node_modules/.test(rPath);
-    });
+      const rPath = path.relative(PJ_PATH, iPath)
+      return !/node_modules/.test(rPath)
+    })
 
-    await extFs.copyFiles(COMMON_PATH, TARGET_COMMON_PATH);
-  });
+    await extFs.copyFiles(COMMON_PATH, TARGET_COMMON_PATH)
+  })
 
   it ('all', async function() {
     const env = {
       config: configPath,
       silent: true
-    };
-    const config = await handler.all({ env });
+    }
+    const config = await handler.all({ env })
 
-    await linkCheck(config);
-    await checkAsyncComponent(config);
-    await checkCssFiles(config);
-  }).timeout(0);
+    await linkCheck(config)
+    await checkAsyncComponent(config)
+    await checkCssFiles(config)
+  }).timeout(0)
 
   it('all --proxy', async function() {
     const env = {
       config: configPath,
       silent: true,
       proxy: true
-    };
-    const config = await handler.all({ env });
+    }
+    const config = await handler.all({ env })
 
-    await linkCheck(config);
-    await checkAsyncComponent(config);
-    await checkCssFiles(config);
-  }).timeout(0);
+    await linkCheck(config)
+    await checkAsyncComponent(config)
+    await checkCssFiles(config)
+  }).timeout(0)
 
   it('all --isCommit', async function() {
     const env = {
       config: configPath,
       silent: true,
       isCommit: true
-    };
-    const config = await handler.all({ env });
+    }
+    const config = await handler.all({ env })
 
-    await linkCheck(config);
-    await checkAsyncComponent(config);
-    await checkCssFiles(config);
-  }).timeout(0);
+    await linkCheck(config)
+    await checkAsyncComponent(config)
+    await checkCssFiles(config)
+  }).timeout(0)
 
   after(async function() {
-    await tUtil.frag.destroy();
-  });
-});
+    await tUtil.frag.destroy()
+  })
+})
